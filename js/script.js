1f6f6243d9bf643f388f9edcf8af47626905e5ff
José-Capucho <joseantoniocapucho@gmail.com>
2023-09-25 15:18:01 -0300

    subindo atualizações no projeto

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const navToggleShowBtn = document.getElementById('navbar-toggler-show');
+const navToggleCloseBtn = document.getElementById('nav-bar-toggler-close');
+const navbarCollapseDiv = document.getElementById('navbar-collapse');
+const navbarDiv = document.querySelector('.navbar');
+
+navToggleShowBtn.addEventListener('click', () => {
+    navbarCollapseDiv.style.display = 'flex';
+});
+
+navToggleCloseBtn.addEventListener('click', () => {
+    navbarCollapseDiv.style.display = 'none';
+});
+
+// hide navbar
+window.addEventListener('scroll', () => {
+
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbarDiv.style.display =  'none';
+    } else {
+        navbarDiv.style.display = '';
+    }
+
+});
